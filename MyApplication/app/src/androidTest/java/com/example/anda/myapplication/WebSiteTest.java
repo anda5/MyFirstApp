5221b52dfd1a9c9524aa738b472b96454bf578fc
Anda.Lixandroiu <lix.anda@yahoo.com>
2015-01-17 00:28:41 +0200

    ....

---
+++ MyApplication/app/src/androidTest/java/com/example/anda/myapplication/WebSiteTest.java
@@ -0,0 +1,23 @@
+package com.example.anda.myapplication;
+
+import junit.framework.TestCase;
+
+import java.lang.instrument.Instrumentation;
+import java.lang.instrument.Instrumentation.*;
+
+/**
+ * Created by anda on 1/13/2015.
+ */
+public class WebSiteTest extends TestCase {
+  public void testWebSiteFullConstructor(){
+    WebSite webSite=new WebSite(z)
+
+
+  }
+  public void testWebSiteGetterAndSetter(){
+
+
+
+  }
+
+}
